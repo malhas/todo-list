c810564d8d9707b5b8b1277ad7a4f5a579e24f33
malhas <luismalheiro1986@gmail.com>
2022-12-30 18:13:47 +0000

    Added new code to project

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-var list = [];
+const date = require(__dirname + '/date.js');
+const list = [];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'))
@@ -10,11 +11,8 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', function(req, res) {
-    var today = new Date();
-    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    var day = today.toLocaleDateString("en-UK", options);
-
-    res.render("list", {day: day, list: list});
+    const day = date.getDate();
+    res.render("list", {listTitle: day, list: list});
 });
 
 app.post('/', function(req, res) {
